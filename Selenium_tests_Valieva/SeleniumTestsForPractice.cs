552be93797d1fee809f4f4ec91f3487a5ecb2326
ValievaKamila <obvx@yandex.ru>
2024-04-14 14:17:54 +0500

    Автотест на авторизацию в стафе

---
+++ Selenium_tests_Valieva/SeleniumTestsForPractice.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Selenium_tests_Valieva;
+
+public class SeleniumTestsForPractice
+{
+    [Test]
+    public void Authorization()
+    {
+        var options = new ChromeOptions();
+        options.AddArguments("--no-sandbox", "--start-maximized", "--disable-extensions");
+        var driver = new ChromeDriver(options);
+        driver.Navigate().GoToUrl("https://staff-testing.testkontur.ru/");
+        Thread.Sleep(3000);
+        var login = driver.FindElement(By.Id("Username"));
+        login.SendKeys("user");
+        var password = driver.FindElement(By.Name("Password"));
+        password.SendKeys("1q2w3e4r%T");
+        Thread.Sleep(3000);
+        var enter = driver.FindElement(By.Name("button"));
+        enter.Click();
+        Thread.Sleep(3000);
+        var currentUrl = driver.Url;
+        Assert.That(currentUrl=="https://staff-testing.testkontur.ru/news");
+        driver.Quit();
+    }
+}
